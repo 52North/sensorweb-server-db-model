e028a4b1ef8b689cb786d7fa7a712db887513c76
CarstenHollmann <c.hollmann@52north.org>
2018-01-12 15:10:16 +0100

    Add referenced observation

---
+++ entities/src/main/java/org/n52/series/db/beans/ReferencedDataEntity.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2015-2018 52°North Initiative for Geospatial Open Source
+ * Software GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.n52.series.db.beans;
+
+import java.util.Collection;
+
+public class ReferencedDataEntity extends DataEntity<String> {
+
+    private static final long serialVersionUID = 4925354534934095827L;
+
+    private String valueIdentifier;
+
+    private String valueName;
+
+    private String valueDescription;
+
+    public String getValueIdentifier() {
+        return valueIdentifier;
+    }
+
+    public void setValueIdentifier(String valueIdentifier) {
+        this.valueIdentifier = valueIdentifier;
+    }
+
+    public String getValueName() {
+        return valueName;
+    }
+
+    public void setValueName(String valueName) {
+        this.valueName = valueName;
+    }
+
+    public String getValueDescription() {
+        return valueDescription;
+    }
+
+    public void setValueDescription(String valueDescription) {
+        this.valueDescription = valueDescription;
+    }
+
+    @Override
+    public boolean isNoDataValue(Collection<String> noDataValues) {
+        String value = getValue();
+        return value == null
+                || noDataValues.contains(value);
+    }
+}
