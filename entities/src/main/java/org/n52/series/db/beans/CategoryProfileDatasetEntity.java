20aa461acb20e15d19e55f9414105ab743973adf
Henning Bredel <h.bredel@52north.org>
2018-02-05 09:15:04 +0100

    some formatting

--- entities/src/main/java/org/n52/series/db/beans/CategoryProfileDatasetEntity.java
+++ entities/src/main/java/org/n52/series/db/beans/CategoryProfileDatasetEntity.java
@@ -1,3 +1,4 @@
+
 package org.n52.series.db.beans;
 
 public class CategoryProfileDatasetEntity extends ProfileDatasetEntity {
--- entities/src/main/java/org/n52/series/db/beans/QuantityProfileDatasetEntity.java
+++ entities/src/main/java/org/n52/series/db/beans/QuantityProfileDatasetEntity.java
@@ -1,3 +1,4 @@
+
 package org.n52.series.db.beans;
 
 public class QuantityProfileDatasetEntity extends ProfileDatasetEntity {
