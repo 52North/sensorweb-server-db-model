3a22784ea29149b795f1e8534edddc4018f304c6
Henning Bredel <bredel@52north.org>
2018-06-15 05:37:01 +0200

    Do querydsl setup via Spring configuration

--- codegen/src/main/java/org/n52/hbm/codegen/Application.java
+++ codegen/src/main/java/org/n52/hbm/codegen/Application.java
@@ -3,10 +3,12 @@ package org.n52.hbm.codegen;
 
 import java.io.File;
 import java.io.IOException;
+import java.sql.Connection;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.inject.Provider;
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
@@ -16,6 +18,7 @@ import org.hibernate.jpa.boot.internal.EntityManagerFactoryBuilderImpl;
 import org.hibernate.jpa.boot.spi.TypeContributorList;
 import org.hibernate.type.BasicType;
 import org.n52.hibernate.type.SmallBooleanType;
+import org.n52.series.db.beans.QFeatureEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
@@ -24,10 +27,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import com.querydsl.jpa.codegen.JPADomainExporter;
+import com.querydsl.sql.Configuration;
+import com.querydsl.sql.SQLQueryFactory;
+import com.querydsl.sql.SQLTemplates;
+import com.querydsl.sql.codegen.SQLCodegenModule;
+import com.querydsl.sql.codegen.SpatialSupport;
+import com.querydsl.sql.spatial.PostGISTemplates;
+import com.querydsl.sql.spring.SpringConnectionProvider;
 
 @SpringBootApplication
 public class Application implements CommandLineRunner {
@@ -35,11 +47,29 @@ public class Application implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
+    
     @Autowired
     @Qualifier("entityManagerFactory")
     private EntityManagerFactory entityManagerFactory;
 
+    @Bean
+    public PlatformTransactionManager transactionManager(DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+    
+    @Bean
+    public Configuration querydslConfiguration() {
+        SQLTemplates templates = PostGISTemplates.builder()
+                .build();
+        return new Configuration(templates);
+    }
+    
+    @Bean
+    public SQLQueryFactory queryFactory(DataSource dataSource) {
+        Provider<Connection> provider = new SpringConnectionProvider(dataSource);
+        return new SQLQueryFactory(querydslConfiguration(), provider);
+    }
+    
     @Bean
     @Primary
     public SessionFactory sessionFactory() {
@@ -59,7 +89,7 @@ public class Application implements CommandLineRunner {
         emf.afterPropertiesSet();
         return emf.getNativeEntityManagerFactory();
     }
-
+    
     private Map<String, Object> addCustomTypes(JpaProperties jpaProperties) {
         Map<String, Object> properties = new HashMap<>(jpaProperties.getProperties());
         properties.put(EntityManagerFactoryBuilderImpl.TYPE_CONTRIBUTORS, createTypeContributorsList());
@@ -80,6 +110,7 @@ public class Application implements CommandLineRunner {
     }
 
     private JPADomainExporter createExporter(String prefix) {
+//        SpatialSupport.addSupport(new SQLCodegenModule());
         return new JPADomainExporter(prefix, new File("src/main/java"), entityManagerFactory.getMetamodel());
     }
 
