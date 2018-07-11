243b1efde05a0f90a101620d882c7dd76afec00e
Henning Bredel <bredel@52north.org>
2018-07-11 13:36:58 +0200

    Make entity abstract

--- entities/src/main/java/org/n52/series/db/beans/ereporting/EReportingProfileDatasetEntity.java
+++ entities/src/main/java/org/n52/series/db/beans/ereporting/EReportingProfileDatasetEntity.java
@@ -19,7 +19,7 @@ package org.n52.series.db.beans.ereporting;
 
 import org.n52.series.db.beans.dataset.ProfileDataset;
 
-public class EReportingProfileDatasetEntity extends EReportingDatasetEntity
+public abstract class EReportingProfileDatasetEntity extends EReportingDatasetEntity
         implements ProfileDataset {
 
     private static final long serialVersionUID = -7514394037111286838L;
