8885b23f472d904cd423e90427ddeeb8e317ee85
CarstenHollmann <c.hollmann@52north.org>
2018-10-12 14:34:15 +0200

    add sql string creation of spatial index

--- dialects/src/main/java/org/n52/hibernate/spatial/dialect/postgis/TimestampWithTimeZonePostgisPG95Dialect.java
+++ dialects/src/main/java/org/n52/hibernate/spatial/dialect/postgis/TimestampWithTimeZonePostgisPG95Dialect.java
@@ -18,6 +18,13 @@
 package org.n52.hibernate.spatial.dialect.postgis;
 
 import java.sql.Types;
+import java.util.HashMap;
+import java.util.Iterator;
+
+import org.hibernate.internal.util.StringHelper;
+import org.hibernate.mapping.Column;
+import org.hibernate.mapping.Index;
+import org.hibernate.mapping.Table;
 
 import org.hibernate.spatial.dialect.postgis.PostgisPG95Dialect;
 
@@ -30,4 +37,38 @@ public class TimestampWithTimeZonePostgisPG95Dialect extends PostgisPG95Dialect 
         registerColumnType(Types.TIMESTAMP, "timestamp with time zone");
     }
 
+    public String buildSqlCreateSpatialIndexString(Index index, String defaultCatalog, String defaultSchema) {
+        // http://postgis.net/docs/manual-2.0/using_postgis_dbmanagement.html#idp60795872
+        // CREATE INDEX [indexname] ON [tablename] USING GIST ( [geometryfield] );
+
+        String name = index.getName();
+        Table table = index.getTable();
+        Iterator<Column> columns = index.getColumnIterator();
+        java.util.Map<Column, String> columnOrderMap = new HashMap<Column, String>();
+
+        StringBuilder buf = new StringBuilder("create")
+                                                       .append(" index ")
+                                                       .append(this.qualifyIndexName()
+                                                               ? name
+                                                               : StringHelper.unqualify(name))
+                                                       .append(" on ")
+                                                       .append(table.getQualifiedName(this,
+                                                                                      defaultCatalog,
+                                                                                      defaultSchema))
+                                                       .append(" USING GIST")
+                                                       .append(" (");
+        while (columns.hasNext()) {
+            Column column = columns.next();
+            buf.append(column.getQuotedName(this));
+            if (columnOrderMap.containsKey(column)) {
+                buf.append(" ")
+                   .append(columnOrderMap.get(column));
+            }
+            if (columns.hasNext()) {
+                buf.append(", ");
+            }
+        }
+        buf.append(")");
+        return buf.toString();
+    }
 }
