9f5fd69b1a0c6cfd9a8c1f7844a64579744855c5
CarstenHollmann <c.hollmann@52north.org>
2018-11-07 15:10:19 +0100

    Enhance PlatformEntity
    Create own table and not same as procedure.

--- entities/src/main/java/org/n52/series/db/beans/PlatformEntity.java
+++ entities/src/main/java/org/n52/series/db/beans/PlatformEntity.java
@@ -21,47 +21,11 @@ import java.util.Set;
 
 import org.n52.series.db.beans.parameter.Parameter;
 
-import com.vividsolutions.jts.geom.Geometry;
-
 public class PlatformEntity extends DescribableEntity {
-
-    public static final String PROPERTY_INSITU = "insitu";
-
-    public static final String PROPERTY_MOBILE = "mobile";
 
     private static final long serialVersionUID = 3615089936334873353L;
 
-    private boolean insitu = true;
-
-    private boolean mobile;
-
-    private Geometry geometry;
-
     private Set<Parameter< ? >> parameters;
-
-    public boolean isMobile() {
-        return mobile;
-    }
-
-    public void setMobile(boolean mobile) {
-        this.mobile = mobile;
-    }
-
-    public boolean isInsitu() {
-        return insitu;
-    }
-
-    public void setInsitu(boolean insitu) {
-        this.insitu = insitu;
-    }
-
-    public Geometry getGeometry() {
-        return geometry;
-    }
-
-    public void setGeometry(Geometry geometry) {
-        this.geometry = geometry;
-    }
 
     @Override
     public Set<Parameter< ? >> getParameters() {
