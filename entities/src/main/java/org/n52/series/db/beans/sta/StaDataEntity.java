371003234697f9e0816ab56f6e374c3578d15041
speckij <speckij@gmail.com>
2020-02-28 12:22:58 +0100

    Add missing constructor to StaDataEntity.
    breaking change as StaDataEntity is now generic instead of extending
    DataEntity<String> only.

--- entities/src/main/java/org/n52/series/db/beans/sta/StaDataEntity.java
+++ entities/src/main/java/org/n52/series/db/beans/sta/StaDataEntity.java
@@ -22,7 +22,7 @@ import org.n52.series.db.beans.AbstractFeatureEntity;
 import org.n52.series.db.beans.DataEntity;
 import org.n52.series.db.beans.sta.StaRelations.Datastream;
 
-public class StaDataEntity extends DataEntity<String> implements Datastream<StaDataEntity> {
+public class StaDataEntity<T> extends DataEntity<T> implements Datastream<StaDataEntity> {
 
     private static final long serialVersionUID = -4720091385202877301L;
 
@@ -30,7 +30,30 @@ public class StaDataEntity extends DataEntity<String> implements Datastream<StaD
 
     private AbstractFeatureEntity<?> featureOfInterest;
 
-    private boolean processsed;
+    private boolean processed;
+
+    public StaDataEntity(DataEntity<T> entity) {
+        super();
+        this.setSamplingTimeStart(entity.getSamplingTimeStart());
+        this.setSamplingTimeEnd(entity.getSamplingTimeEnd());
+        this.setValue(entity.getValue());
+        this.setGeometryEntity(entity.getGeometryEntity());
+        this.setDeleted(entity.getDeleted());
+        this.setValidTimeStart(entity.getValidTimeStart());
+        this.setValidTimeEnd(entity.getValidTimeEnd());
+        this.setResultTime(entity.getResultTime());
+        this.setParent(entity.getParent());
+        this.setDataset(entity.getDataset());
+        this.setRelatedObservations(entity.getRelatedObservations());
+        this.setValueIdentifier(entity.getValueIdentifier());
+        this.setValueName(entity.getValueName());
+        this.setValueDescription(entity.getValueDescription());
+        this.setVerticalFrom(entity.getVerticalFrom());
+        this.setVerticalTo(entity.getVerticalTo());
+        this.setDetectionLimit(entity.getDetectionLimit());
+        this.setSamplingProfile(entity.getSamplingProfile());
+        this.setEreportingProfile(entity.getEreportingProfile());
+    }
 
     @Override
     public DatastreamEntity getDatastream() {
@@ -55,12 +78,12 @@ public class StaDataEntity extends DataEntity<String> implements Datastream<StaD
         return getFeatureOfInterest() != null;
     }
 
-    public boolean isProcesssed() {
-        return processsed;
+    public boolean isProcessed() {
+        return processed;
     }
 
-    public void setProcesssed(boolean processsed) {
-        this.processsed = processsed;
+    public void setProcessed(boolean processed) {
+        this.processed = processed;
     }
 
     @Override
